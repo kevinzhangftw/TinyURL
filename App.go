c0760bc08c8201e689320dd41735e8f057ec6143
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2018-12-28 11:20:18 -0800

    saving to db stubbed

--- App.go
+++ App.go
@@ -13,8 +13,9 @@ func processURL(inputURL string) (tinyURL string){
 	return fmt.Sprintf("%x", bs[:4])
 }
 
-func saveURL(tinyURL string){
+func saveURL(tinyURL string) (err error){
 	fmt.Println("Hello, 世界")
+	return nil
 }
 
 func routeTo(w http.ResponseWriter, r *http.Request){
@@ -22,7 +23,6 @@ func routeTo(w http.ResponseWriter, r *http.Request){
 	case "GET":
 		fmt.Fprint(w, "Welcome to TinyUrl, this is a server, why dont you try to make a request?")
 		fmt.Fprint(w, "request URL is ", r.URL)
-
 
 	case "POST":
 		if err := r.ParseForm(); err != nil {
@@ -33,12 +33,12 @@ func routeTo(w http.ResponseWriter, r *http.Request){
 		inputURL := r.FormValue("inputURL")
 
 		tinyURL := processURL(inputURL)
-		//if err := saveURL(tinyURL); err != nil {
-		//	fmt.Fprintf(w, "saveURL err: %v", err)
-		//	return
-		//}
+		if err := saveURL(tinyURL); err != nil {
+			fmt.Fprintf(w, "saveURL err: %v", err)
+			return
+		}
 
-		fmt.Fprintf(w, "tinyURL = %s\n", tinyURL)
+		fmt.Fprintf(w, "tinyURL is %s\n", tinyURL)
 
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
